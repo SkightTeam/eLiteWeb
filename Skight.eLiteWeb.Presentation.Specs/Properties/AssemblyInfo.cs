b80e3c861c1674d8ea5c857eaeb47e7f49a9dd48
Hao Wang <HWonner@Gmail.Com>
2013-01-23 16:44:41 -0700

    Update Skight.eLiteWeb.Presentation.Specs/Properties/AssemblyInfo.cs

--- Skight.eLiteWeb.Presentation.Specs/Properties/AssemblyInfo.cs
+++ Skight.eLiteWeb.Presentation.Specs/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Skight.LightWeb.Presentation.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GOA")]
+[assembly: AssemblyCompany("Skight I-Tech Inc.")]
 [assembly: AssemblyProduct("Skight.LightWeb.Presentation.Specs")]
-[assembly: AssemblyCopyright("Copyright © GOA 2012")]
+[assembly: AssemblyCopyright("Copyright © Skight I-Tech Inc. 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
