59afe257452c4b54f91ca9c4de0921e9f67809d3
Hao Wang <HWonner@gmail.com>
2013-06-27 16:18:07 -0600

    Add FluentBuild tool and project for it

---
+++ Skight.HelpCenter.Build/Default.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentBuild;
+
+namespace Skight.HelpCenter.Build {
+    public class Default:BuildFile 
+    {
+        public Default()
+        {
+            AddTask("Start",Start);
+        }
+        void Start()
+        {
+            Console.WriteLine("Start Step");
+        }
+    }
+}
