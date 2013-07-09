8b4575100b1fa5cc741991271c57e9d70e3acc95
Hao Wang <HWonner@gmail.com>
2013-07-09 16:34:26 -0600

    Add more compile steps

--- Skight.HelpCenter.Build/Default.cs
+++ Skight.HelpCenter.Build/Default.cs
@@ -5,23 +5,43 @@ using FluentFs.Core;
 namespace Skight.HelpCenter.Build {
     public class Default:BuildFile 
     {
+        private static Directory publish_directory =
+            new Directory(Properties.CurrentDirectory).SubFolder("Publish");
+        private static Directory bin_direcotry = publish_directory.SubFolder("bin");
+        private static Directory helper_direcotry = publish_directory.SubFolder("HelperCenter");
         public Default()
         {
-            AddTask(Prepare);
-            AddTask(Compile);
+            AddTask(prepare);
+            AddTask(compile_elite_web);
+            AddTask(compile_helper_center);
         }
 
-        void Prepare()
+        void prepare()
         {
-            var base_directory = new Directory(Properties.CurrentDirectory);
-            base_directory.SubFolder("Publish").SubFolder("bin").Create();
-            base_directory.SubFolder("Publish").SubFolder("HelpCenter").Create();
+            publish_directory.Delete(OnError.Continue).Create();
+            bin_direcotry.Create();
+            helper_direcotry.Create();
         }
-        void Compile()
+        void compile_elite_web()
         {
             Task.Build.Csc.Target.Library(t=>t.AddResources(
                 new FileSet().Include(new File(@"Skight.eLiteWeb.Domain\\**\*.cs")))
-                .OutputFileTo(@"Publish\bin\Domain.dll"));
+                .OutputFileTo(bin_direcotry.File("eLiteWeb.Domain.dll")));
+            Task.Build.Csc.Target.Library(t => t.AddResources(
+               new FileSet().Include(new File(@"Skight.eLiteWeb.Presentation\\**\*.cs")))
+               .OutputFileTo(bin_direcotry.File("ELiteWeb.Presentation.dll")));
+            Task.Build.Csc.Target.Library(t => t.AddResources(
+             new FileSet().Include(new File(@"Skight.eLiteWeb.Applicaiton\\**\*.cs")))
+             .OutputFileTo(bin_direcotry.File("ELiteWeb.Application.dll")));
+        }
+        void compile_helper_center()
+        {
+            Task.Build.Csc.Target.Library(t => t.AddResources(
+                new FileSet().Include(new File(@"Skight.HelpCenter.Domain\\**\*.cs")))
+                .OutputFileTo(bin_direcotry.File("HelpCenter.Domain.dll")));
+            Task.Build.Csc.Target.Library(t => t.AddResources(
+               new FileSet().Include(new File(@"Skight.HelpCenter.Presentation\\**\*.cs")))
+               .OutputFileTo(bin_direcotry.File("HelpCenter.Presentation.dll")));
         }
     }
 }
