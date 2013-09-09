ba5eea66cd72041f28740cdd5b2e2b46d66de4bd
Hao Wang <HWonner@gmail.com>
2013-09-09 15:42:26 -0600

    Fix unit tests for NameConventionFilter

--- Skight.eLiteWeb.Presentation.Specs/Web/CommandFilters/NameConventionFilterSpecs.cs
+++ Skight.eLiteWeb.Presentation.Specs/Web/CommandFilters/NameConventionFilterSpecs.cs
@@ -1,5 +1,4 @@
-﻿using log4net.Filter;
-using Machine.Specifications;
+﻿using Machine.Specifications;
 using Rhino.Mocks;
 using Skight.eLiteWeb.Presentation.Web.FrontControllers;
 
@@ -7,30 +6,34 @@ namespace Skight.eLiteWeb.Presentation.Web.CommandFilters
 {
     public class NameConventionFilterSpecs
     {
-         
-    }
+        private Establish context =
+            () =>request = MockRepository.GenerateStub<WebRequest>();
 
-    public class regualr_get_command
-    {
-        Establish context = () =>
-        {
-            filter = new NameConventionFilter(new CommandName());
-            request = MockRepository.GenerateStub<WebRequest>();
-           
-        };
+        Because of = () => filter = new NameConventionFilter(new CommandName());
 
-        Because of = () => { request.Stub(x => x.Input.RequestPath).Return("CommandName"); };
-
-        It should_can_process = () => filter.can_process(request).ShouldBeTrue();
-        private static NameConventionFilter filter;
-        private static WebRequest request;
-
-        class CommandName:DiscreteCommand
+        protected static NameConventionFilter filter;
+        protected static WebRequest request;
+        private class CommandName : DiscreteCommand
         {
             public void process(WebRequest request)
             {
                 throw new System.NotImplementedException();
             }
         }
+    }
+
+    public class when_request_exactly_matched_url_to_name_convention_filter : NameConventionFilterSpecs
+    {
+        Establish context = () => request.Stub(x => x.Input.RequestPath).Return("CommandName.do");
+        It should_can_process = () => filter.can_process(request).ShouldBeTrue();
+    }
+
+    //public class when_request_matched_url_with_query_paramters_to_name_convention_filter : NameConventionFilterSpecs {
+    //    Establish context = () => request.Stub(x => x.Input.RequestPath).Return("CommandName.do?query=test");
+    //    It should_can_process = () => filter.can_process(request).ShouldBeTrue();
+    //}
+    public class when_request_not_matched_url_to_name_convention_filter : NameConventionFilterSpecs {
+        Establish context = () => request.Stub(x => x.Input.RequestPath).Return("WrongName.do");
+        It should_can_process = () => filter.can_process(request).ShouldBeFalse();
     }
 }
