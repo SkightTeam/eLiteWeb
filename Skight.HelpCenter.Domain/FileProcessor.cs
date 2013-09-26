f93826c0364fb83a6d62fa08e89ee69b14a7f5b1
Hao Wang <HWonner@gmail.com>
2013-09-26 11:11:32 -0600

    FFP break condistion chains

--- Skight.HelpCenter.Domain/FileProcessor.cs
+++ Skight.HelpCenter.Domain/FileProcessor.cs
@@ -12,17 +12,16 @@ namespace Skight.HelpCenter.Domain
             foreach (var file in files)
             {
                 FileInfo file_info=new FileInfo(file);
-                if (file_info.Name.StartsWith("Test"))
-                {
-                    if (file_info.Extension==".pdf")
-                    {
-                        if (!file_info.IsReadOnly)
-                        {
-                            Console.WriteLine("Processing {0}",file);
-                        }
-                    }
-                }
+                if (!file_info.Name.StartsWith("Test")) 
+                    continue;
 
+                if (file_info.Extension != ".pdf") 
+                    continue;
+
+                if (!file_info.IsReadOnly)
+                {
+                    Console.WriteLine("Processing {0}",file);
+                }
             }
         }
     }
